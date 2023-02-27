47543003ae52cb127b4a38233fee13099cd2f0e2
jiyasavaliya18 <126229948+jiyasavaliya18@users.noreply.github.com>
2023-02-27 19:50:08 +0530

    Add files via upload

---
+++ wap to find min no if else.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+	int a,b;
+	
+	printf("enter a\n");
+	scanf("%d",&a);
+	
+	printf("enter b\n");
+	scanf("%d",&b);
+	
+	if(a<b){
+		printf("A\n");
+	}
+	
+	else{
+		printf("B\n");
+	}
+	return 0;
+}
